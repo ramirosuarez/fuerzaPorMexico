ced3875772ebe867198be6e02a2dcb3aa9e413b9
ramirosuarez <ramiro-suarez-95@hotmail.com>
2020-12-21 10:52:44 -0600

    Se adapato al proyeto

--- routes/siteRoutes.js
+++ routes/siteRoutes.js
@@ -1,10 +1,108 @@
 const express = require('express')
 const router = express.Router()
 
-//Se agrega la confuracion de la base de datos
+/*  Se agrega la confuracion de la base de datos  */
 const admin = require('firebase-admin')
 const db = admin.firestore()
 const firebase = require('firebase')
+
+/* */
+router.get('/', (req, res) => {
+  res.render('ladingPage', {
+
+  })
+})
+
+/** creacion de usuarios-[signup] */
+router.get('/signup', (req, res) => {
+  res.render('signup', {
+  })
+})
+router.post('/signup', function (req, res) {
+  console.log(req.body)
+  
+  let email = req.body.email
+  let password = req.body.password
+  
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+  router.get('/login', (req, res) => {
+    res.render('login', {
+    })
+  })
+    .then((user) => {
+      // Signed in
+      // ...
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log('contraseña corta')
+      // ..
+    });
+
+  res.redirect('/home')
+
+})
+
+
+/**inicio de sesion- [login]  */
+router.get('/login', (req, res) => {
+  res.render('login', {
+  })
+})
+router.post('/login', function (req, res) {
+  console.log(req.body)
+
+  let email = req.body.email
+  let password = req.body.password
+
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then((user) => {
+      console.log('has iniciad');
+      res.redirect('/home')
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+    });
+})
+
+/** Cerrar sesion - [logout]*/
+router.get('/logout', function (req, res) {
+  firebase.auth().signOut().then(function () {
+    // Sign-out successful.
+    console.log('cerraste session')
+  }).catch(function (error) {
+    // An error happened.
+  });
+  res.redirect('/login')
+})
+/* formulario de Agremiado */
+router.get('/addAgremiado', (req, res) => {
+  res.render('addAgremiado', {
+  })
+})
+
+router.post('/addAgremiado', async (req, res) => {
+  console.log(req.body)
+  await db.collection('Afiliados').add(req.body)
+  res.redirect('/dash')
+})
+
+
+
+router.get('/dash', async (req, res) => {
+  const data = []
+  const afiliados = await db.collection('Afiliados').get()
+  for (const afiliado of afiliados.docs) {
+    data.push(afiliado.data())
+  }
+  res.render('dash', {
+    afiliados: data
+  })
+})
+
+module.exports = router
 
 
 // router.post('/home', async(req, res) => {
@@ -14,10 +112,6 @@ const firebase = require('firebase')
 // })
 
 
-router.get('/', (req, res) => {
-    res.render('signup',{
-    })
-})
 
 // router.post('/signup', async (req,res) => {
 //     console.log(req.body)
@@ -25,98 +119,17 @@ router.get('/', (req, res) => {
 //     res.redirect('/home')
 // })
 
-
-///////////////////////////////////////////
-/**configuracion para ingresar usuarios  */
-///////////////////////////////////////////
-router.post('/signup', function (req,res) {
-    console.log(req.body)
-
-    let email = req.body.email
-    let password = req.body.password
-    
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-      .then((user) => {
-        // Signed in
-        // ...
-      })
-      .catch((error) => {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        console.log('contraseña corta')
-        // ..
-      });
-
-    res.redirect('/home')
-   
-})
-
-
-router.get('/signup', (req, res) => {
-    res.render('signup',{
-    })
-})
-
+// router.post('/signup', async (req,res) => {
+//     console.log(req.body)
+//     await db.collection('Afiliados').add(req.body)
+//     res.redirect('/home')
+// })
 /**
- * login
+ * Agregar un dato a la base de dato en firebase
  */
-router.get('/login',(req,res)=>{
-    res.render('login',{
-    })
-})
 
-router.post('/login', function(req, res) {
-  console.log(req.body)
-
-  let email = req.body.email
-  let password = req.body.password
-
-  firebase.auth().signInWithEmailAndPassword(email, password)
-      .then((user) => {
-        console.log('has iniciad');
-          res.redirect('/home')
-      })
-      .catch((error) => {
-          var errorCode = error.code;
-          var errorMessage = error.message;
-      });
-
-
-})
-
-/**
- * cerrar sesion
- */
-router.get('/logout', function (req, res) {
-  firebase.auth().signOut().then(function() {
-      // Sign-out successful.
-      console.log('cerraste session')
-    }).catch(function(error) {
-      // An error happened.
-    });
-
-  res.redirect('/login')
-})
 ///////////////////////////////
 // router.get('/home',(req,res)=>{
 //     res.render('home',{
 //     })
 // })
-
-
-
-router.get('/home',(req,res)=>{
-    res.render('ladingPage')
-})
-// router.get('/tabla', async(req, res) => {
-//         const data = []
-//         const pruevaMuchas = await db.collection('prueva').get()
-//         for (const prueva of pruevaMuchas.docs) {
-//             data.push(prueva.data())
-//         }
-//         res.render('muestra', {
-//             pruevas: data
-//         })
-//     })
-    ///
-module.exports = router
